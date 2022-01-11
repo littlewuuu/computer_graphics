3b9cbb95acb3583cead9fc0ecf4f623c7e9fbb8e
littlewuuu <2622632937@qq.com>
2022-01-11 22:20:49 +0100

    homework

--- pa0/main.cpp
+++ pa0/main.cpp
@@ -4,45 +4,25 @@
 #include<iostream>
 
 int main(){
+    Eigen::Vector3f point(2.0f,1.0f,1.0f); //use homogenous coordinates, so the third element is 1.0f
+    
+    Eigen::Matrix3f rota;
+    Eigen::Matrix3f tran;
+    double theta = 45.0/180.0*M_PI; 
+    
+    rota <<cos(theta),-1.0*sin(theta),0,
+            sin(theta),cos(theta),0,
+            0,0,1;
 
-    // Basic Example of cpp
-    std::cout << "Example of cpp \n";
-    float a = 1.0, b = 2.0;
-    std::cout << a << std::endl;
-    std::cout << a/b << std::endl;
-    std::cout << std::sqrt(b) << std::endl;
-    std::cout << std::acos(-1) << std::endl;
-    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
+    tran <<1,0,1,
+    0,1,2,
+    0,0,1;
 
-    // Example of vector
-    std::cout << "Example of vector \n";
-    // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
-    // vector output
-    std::cout << "Example of output \n";
-    std::cout << v << std::endl;
-    // vector add
-    std::cout << "Example of add \n";
-    std::cout << v + w << std::endl;
-    // vector scalar multiply
-    std::cout << "Example of scalar multiply \n";
-    std::cout << v * 3.0f << std::endl;
-    std::cout << 2.0f * v << std::endl;
+    point = tran*rota*point;
+    std::cout<<point<<std::endl;
+    std::cout<<point[0]<<","<<point[1]<<std::endl;
 
-    // Example of matrix
-    std::cout << "Example of matrix \n";
-    // matrix definition
-    Eigen::Matrix3f i,j;
-    i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
-    j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
-    // matrix output
-    std::cout << "Example of output \n";
-    std::cout << i << std::endl;
-    // matrix add i + j
-    // matrix scalar multiply i * 2.0
-    // matrix multiply i * j
-    // matrix multiply vector i * v
 
+   
     return 0;
 }
